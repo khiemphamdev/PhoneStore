7172d5fd20898bb5915168b9becd67b014b23aa4
khiemphamdev <khiempham.dev@gmail.com>
2026-05-31 10:05:47 +0700

    set up

---
+++ src/test/java/ntu/phamgiakhiem/PhoneStore/PhoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.phamgiakhiem.PhoneStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
