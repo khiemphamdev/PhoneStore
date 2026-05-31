df8e8098c4ef6ca5295c235ecb3d005a7b038981
khiemphamdev <khiempham.dev@gmail.com>
2026-05-31 10:11:57 +0700

    Viết model user, role

---
+++ src/main/java/ntu/phamgiakhiem/PhoneStore/config.java
@@ -0,0 +1,5 @@
+package ntu.phamgiakhiem.PhoneStore;
+
+public class config {
+
+}
---
+++ src/main/java/ntu/phamgiakhiem/PhoneStore/models/Role.java
@@ -0,0 +1,40 @@
+package ntu.phamgiakhiem.PhoneStore.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;  // Ví dụ: ROLE_USER, ROLE_ADMIN
+//getter, setter, contrstructor
+
+	public Role(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
+}
